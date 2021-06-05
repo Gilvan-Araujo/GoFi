acb2c1380cdb4cabb5cea8c5a9d57c48df71ddaf
Gilvan <45008443+Gilvan-Araujo@users.noreply.github.com>
2021-06-05 10:12:17 -0300

    :tada: Challenge completed

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,5 +1,7 @@
-import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
+
+import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
 
@@ -16,17 +18,28 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
+  const [image, setImage] = useState('');
 
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string): void {
+    setImage(url);
+    onOpen();
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing="40px">
+        {cards.map(card => (
+          <Card
+            key={card.id}
+            data={card}
+            viewImage={() => handleViewImage(card.url)}
+          />
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} onClose={onClose} imgUrl={image} />
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,92 @@
-import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
-import { Header } from '../components/Header';
+import { Box, Button } from '@chakra-ui/react';
+
 import { CardList } from '../components/CardList';
-import { api } from '../services/api';
-import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
+import { Header } from '../components/Header';
+import { Loading } from '../components/Loading';
+import { api } from '../services/api';
+
+type IDataRequestProps = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+};
+
+interface IRequest {
+  data: Array<IDataRequestProps>;
+  after: string;
+}
 
 export default function Home(): JSX.Element {
+  const getImages = async ({ pageParam = 0 }): Promise<IRequest> => {
+    const response = await api.get(`/api/images`, {
+      params: {
+        after: pageParam,
+      },
+    });
+    return response.data;
+  };
+
+  const presetData = {
+    "pages": [
+      {
+        "data": [
+          {
+            "title": "Doge",
+            "description": "The best doge",
+            "url": "https://i.ibb.co/K6DZdXc/minh-pham-LTQMgx8t-Yq-M-unsplash.jpg",
+            "ts": 1620222828340000,
+            "id": "294961059684418048"
+          },
+          {
+            "title": "Cachorrinho gif",
+            "description": "A Gracie é top",
+            "url": "https://i.ibb.co/r3NbmgH/ezgif-3-54a30c130cef.gif",
+            "ts": 1620222856980000,
+            "id": "295991055792210435"
+          },
+          {
+            "title": "React",
+            "description": "Dan Abramov",
+            "url": "https://i.ibb.co/864qfG3/react.png",
+            "ts": 1620223108460000,
+            "id": "295991069654385154"
+          },
+          {
+            "title": "Ignite",
+            "description": "Wallpaper Celular",
+            "url": "https://i.ibb.co/DbfGQW5/1080x1920.png",
+            "ts": 1620223119610000,
+            "id": "295991085899973123"
+          },
+          {
+            "title": "Ignite",
+            "description": "Wallpaper PC 4k",
+            "url": "https://i.ibb.co/fvYLKFn/3840x2160.png",
+            "ts": 1620223133800000,
+            "id": "295991107279389188"
+          },
+          {
+            "title": "Paisagem",
+            "description": "Sunset",
+            "url": "https://i.ibb.co/st42sNz/petr-vysohlid-9fqw-Gq-GLUxc-unsplash.jpg",
+            "ts": 1620223149390000,
+            "id": "295991128736399874"
+          }
+        ],
+        "after": "295991160078336512"
+      }
+    ],
+    "pageParams": [
+      null
+    ]
+  }
+
   const {
     data,
     isLoading,
@@ -18,18 +96,34 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    getImages,
+    {
+      getNextPageParam: result => {
+        const { after } = result;
+        if (after) {
+          return after;
+        }
+        return null;
+      },
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    if (data) {
+      // return presetData.pages.map(page => page.data).flat?
+      return data.pages.map(page => page.data).flat(2);
+    }
+
+    return null;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +131,11 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button mt="32px" onClick={() => fetchNextPage()}>
+            {isFetchingNextPage ? 'Carregando' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
